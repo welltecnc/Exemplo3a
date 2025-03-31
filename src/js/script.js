348231e79eda71ac5acd10bbe79e71069d6c77b6
Wellington <welltecnic@hotmail.com>
2025-03-31 09:13:41 -0300

    tipos de variaveis-typeof

--- src/js/script.js
+++ src/js/script.js
@@ -17,4 +17,21 @@ console.log(nome1)
 let nome2=null;
 console.log(nome2)
 
+//TIPOS DE VARIAVEIS
+
+let exemplo1 =10;
+console.log(typeof exemplo1)
+
+let exemplo2 ="Tecnologia";
+console.log(typeof exemplo2)
+
+let exemplo3 =true;
+console.log(typeof exemplo3)
+
+let exemplo4={};
+console.log(typeof exemplo4)
+
+let exemplo5 =[];
+console.log(typeof exemplo5)
+
 
