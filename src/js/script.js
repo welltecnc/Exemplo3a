c5fa893d255de64971d32334970879b97d9157ef
Wellington <welltecnic@hotmail.com>
2025-03-31 09:07:43 -0300

    declaração e variaveis

---
+++ src/js/script.js
@@ -0,0 +1,20 @@
+//DECLARARAÇÕES E VARIAVEIS
+
+var nome ="fiap";
+console.log(nome);
+
+let idade = 19;
+console.log(idade)
+
+const sobrenome="Fiapinho";
+console.log(sobrenome)
+
+//declarando uma variavel indefinida
+let nome1;
+console.log(nome1)
+
+//declarando uma variavel nula
+let nome2=null;
+console.log(nome2)
+
+
